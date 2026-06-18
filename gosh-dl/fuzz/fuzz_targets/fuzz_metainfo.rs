cc6f9649ac492668948e3604b0e78a097350905a
MrShellad <chris@cabeu.edu.kg>
2026-06-18 11:54:22 +0800

    feat: initialize Tauri project with Cargo manifest and gitignore configuration

--- gosh-dl/fuzz/fuzz_targets/fuzz_metainfo.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use gosh_dl::torrent::Metainfo;
-
-fuzz_target!(|data: &[u8]| {
-    // parse() should never panic on arbitrary input
-    let _ = Metainfo::parse(data);
-});
